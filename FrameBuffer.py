b66f4ef49c13c2b9487c5d305a28db1ecfa0c814
lyj00912 <3122742760@qq.com>
2024-08-01 13:20:27 +0800

    created M02/C01, also created fb.display_debug() to draw white lines between each pixels, make rasterizer debugging easier.

--- FrameBuffer.py
+++ FrameBuffer.py
@@ -26,3 +26,22 @@ class FrameBuffer:
         plt.imshow(self.buffer)
         plt.axis('off')  # Hide axis
         plt.show()
+
+    def display_debug(self):
+        # 获取原始图像的高度和宽度
+        height, width, channels = self.buffer.shape
+
+        # 创建一个新的6倍分辨率的图像，初始为全白色
+        new_height = height * 6
+        new_width = width * 6
+        new_buffer = np.ones((new_height, new_width, channels), dtype=np.uint8) * 255
+
+        # 遍历每个像素，填充到新图像中
+        for y in range(height):
+            for x in range(width):
+                # 将原像素的颜色复制到6x6块的左上角5x5区域
+                new_buffer[y * 6:y * 6 + 5, x * 6:x * 6 + 5] = self.buffer[y, x]
+
+        plt.imshow(new_buffer)
+        plt.axis('off')  # 隐藏坐标轴
+        plt.show()
